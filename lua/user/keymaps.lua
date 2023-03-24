6891bd28a7e2b1240d9eeb01d10b7c74442a95d6
Youtirsin <youtirsin@qq.com>
2023-03-24 14:22:53 +0800

    lsp settings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -35,10 +35,6 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
 -- Insert --
 -- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)
@@ -48,18 +44,6 @@ keymap("i", "kj", "<ESC>", opts)
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -16,27 +16,39 @@ if not lspconfig_status_ok then
 	return
 end
 
- require("mason-lspconfig").setup_handlers({
-     -- The first entry (without a key) will be the default handler
-     -- and will be called for each installed server that doesn't have
-     -- a dedicated handler.
-     function (server_name) -- default handler (optional)
-         require("lspconfig")[server_name].setup {}
-     end,
-     -- Next, you can provide targeted overrides for specific servers.
-     -- ["rust_analyzer"] = function ()
-     --     require("rust-tools").setup {}
-     -- end,
-     -- ["lua_ls"] = function ()
-     --     lspconfig.lua_ls.setup {
-     --         settings = {
-     --             Lua = {
-     --                 diagnostics = {
-     --                     globals = { "vim" }
-     --                 }
-     --             }
-     --         }
-     --     }
-     -- end,
- })
+local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_cmp_ok then
+	return
+end
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+require("mason-lspconfig").setup_handlers({
+   -- The first entry (without a key) will be the default handler
+   -- and will be called for each installed server that doesn't have
+   -- a dedicated handler.
+   function (server_name) -- default handler (optional)
+      require("lspconfig")[server_name].setup {
+        capabilities = capabilities 
+      }
+   end,
+   -- Next, you can provide targeted overrides for specific servers.
+   -- ["rust_analyzer"] = function ()
+   --     require("rust-tools").setup {}
+   -- end,
+   -- ["lua_ls"] = function ()
+   --     lspconfig.lua_ls.setup {
+   --         settings = {
+   --             Lua = {
+   --                 diagnostics = {
+   --                     globals = { "vim" }
+   --                 }
+   --             }
+   --         }
+   --     }
+   -- end,
+})
+
 
