21a26881113e9ea4b21291712d94730c01cb0e37
Youtirsin <youtirsin@qq.com>
2023-04-12 20:56:52 +0800

    fix luasnip jumping

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -17,19 +17,43 @@ cmp.setup {
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-  window =  {
+  window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(), --youtirsin
-		["<C-j>"] = cmp.mapping.select_next_item(), --youtirsin
+    ["<C-j>"] = cmp.mapping.select_next_item(), --youtirsin
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    -- for luasnip, config from nvim-cmp --youtirsin
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
+
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
@@ -55,4 +79,3 @@ cmp.setup.cmdline({ '/', '?' }, {
 --     { name = 'cmdline' }
 --   })
 -- })
-
