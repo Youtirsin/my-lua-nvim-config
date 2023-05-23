7d9b0d110761c110d4fb2b3defbb26750d00b42b
youtirsin <youtirsin@qq.com>
2023-05-23 11:07:57 +0800

    use coc autopair

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,7 +56,6 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope.nvim" -- requires ripgrep, apt install ripgrep
   use "nvim-treesitter/nvim-treesitter"
   use "phaazon/hop.nvim"
-  use "windwp/nvim-autopairs"
 
   -- Colorschemes
   use "navarasu/onedark.nvim"
