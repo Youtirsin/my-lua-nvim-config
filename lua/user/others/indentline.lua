ef6507ff168b03a5bf9c345837dd3add8eec2439
youtirsin <youtirsin@qq.com>
2023-10-13 19:10:12 +0800

    update indentline config

--- lua/user/others/indentline.lua
+++ lua/user/others/indentline.lua
@@ -1,12 +1,7 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
-vim.opt.list = true
+indent_blankline.setup()
 
-require("indent_blankline").setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-}
